574776ac53fc37b97ce453aa50946c6baa573f81
poskryobik <poskrebyshev17@yandex.ru>
2024-01-07 16:31:55 +0300

    Change dockerfile

--- app/run.sh
+++ app/run.sh
@@ -2,4 +2,4 @@ docker container stop api_app
 docker container rm api_app
 docker build -t api_app -f Dockerfile .
 sleep 3
-docker run -d --network host --restart=always --name api_app api_app
+docker run -d --name api_app -p 8000:80 api_app
