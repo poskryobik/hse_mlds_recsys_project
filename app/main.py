f9a7678f514a31bcef3e78de9bbd80535876cc5e
poskryobik <poskrebyshev17@yandex.ru>
2024-03-10 15:28:14 +0300

    Add redis cash and docker compose

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile,  WebSocket
 from fastapi.responses import FileResponse
 from typing import List
 from pydantic import BaseModel
 import pickle
 import pandas as pd
-from lightfm import LightFM
 from LFM import LFM
-
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
+import datetime
 
 
 
@@ -26,14 +29,36 @@ products_df = pd.read_csv("products.csv", sep=',', encoding='utf-8')
 lfm_model = LFM(model=model, rating_df=rating_df, encoder=encoder)
 
 
+
+@app.get("/")
+@cache(expire=60)
+async def greetings():
+    """ 
+        Ручка для приветсвия 
+    """
+    return "Hello! This API for ML Service"
+
+
+@app.on_event("startup")
+async def startup():
+    """
+        Инициализация Redis
+    """
+    redis = aioredis.from_url("redis://localhost")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
+
 class User(BaseModel):
     uid: int
 
 
 @app.get("/personal_reccomend")
+@cache(expire=120) # Кеширование 2 минуты
 def get_personal_reccom(user: User):
     """ 
         Персональная рекомендация
+        (с кешированием 2 мин.)
     """
     items = lfm_model.recommend(uid=user.uid, k=10)
     products_name = products_df[products_df["product_id"].isin(items)]["product_name"].to_list()
@@ -41,9 +66,11 @@ def get_personal_reccom(user: User):
 
 
 @app.get("/multiple_reccomend")
+@cache(expire=120) # Кеширование 2 минуты
 def get_multiple_reccom(users: List[User]):
     """ 
         Для списка пользователей возвращает рекомендации
+        (с кешированием 2 мин.)
     """
     users_ids = [user.uid for user in users]
     prediction = lfm_model.predict(users_to_recommend=users_ids, k=10)
@@ -70,3 +97,12 @@ def create_upload_file(file: UploadFile):
     df = df.merge(rec_df, on="user_id", how="left")
     df.to_csv("multiple_reccomend.csv")
     return FileResponse("multiple_reccomend.csv", media_type="text/csv", filename=file.filename)
+
+
+@app.get("/ping")
+async def ping():
+    """
+        Проверка доступности сервера
+    """
+    now = datetime.datetime.now()
+    return f"Server is available. Server date: {now}"
